76ccaa81f30de4cda5f75bb2642b0538b7f4b723
Seong_gu <sunggu507@naver.com>
2024-08-08 17:29:38 +0900

    feat : 컨트롤러에서 remove modify 매핑 후 DB처리

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -3,7 +3,11 @@ package org.zerock.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -17,13 +21,52 @@ public class BoardController {
 
 	private BoardService service;
 	
-	@GetMapping("register")
-	public void register() {
-		
-	}
 	
 	@GetMapping("list")
 	public void list(Model model) {
 		model.addAttribute("list", service.getList());
 	}
+	
+	
+	@GetMapping("register")
+	public void register() {
+		
+	}
+	
+	@PostMapping("register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		service.register(board);
+		log.info("register : " + board);
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping({"get", "modify"})
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		log.info("/get or /modify..........");
+		
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		if(service.remove(bno)) { 
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list";
+	}
+	
+	
 }
