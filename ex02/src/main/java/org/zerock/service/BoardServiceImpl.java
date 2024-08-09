fc950cc6f464cab49c157d9458ac3c6752b09524
Seong_gu <sunggu507@naver.com>
2024-08-09 11:38:59 +0900

    feat : pageing 처리

--- ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.mapper.BoardMapper;
 
 import lombok.AllArgsConstructor;
@@ -25,9 +26,9 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 	@Override
-	public List<BoardVO> getList() {
+	public List<BoardVO> getList(Criteria cri) {
 		log.info("getList...............");
-		return mapper.getList();
+		return mapper.getListWithPaging(cri);
 	}
 
 	@Override
