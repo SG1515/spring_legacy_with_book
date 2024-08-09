7d4c74fd1fd18e1dc9a9c22bba05744a38c652db
Seong_gu <sunggu507@naver.com>
2024-08-09 10:43:07 +0900

    feat : paging 처리 테스트

--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -2,12 +2,15 @@ package org.zerock.mapper;
 
 import static org.junit.Assert.fail;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 import lombok.extern.log4j.Log4j;
 
@@ -19,6 +22,16 @@ public class BoardMapperTests {
 
 	@Autowired
 	private BoardMapper mapper;
+	
+	@Test
+	public void testPaing() {
+		Criteria cri = new Criteria();
+		cri.setPageNum(1);
+		cri.setAmount(10);
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		list.forEach(board -> log.info(board));
+	}
 	
 //	@Test
 //	public void test() {
@@ -49,15 +62,15 @@ public class BoardMapperTests {
 //	}
 
 	
-	@Test
-	public void testUpdate() { 
-		BoardVO board = new BoardVO();
-		board.setBno(4L);
-		board.setTitle("수정된 제목");
-		board.setContent("수정된 내용");
-		board.setWriter("user00");
-		
-		int count = mapper.update(board);
-		log.info("Update count : " + count);
-	}
+//	@Test
+//	public void testUpdate() { 
+//		BoardVO board = new BoardVO();
+//		board.setBno(4L);
+//		board.setTitle("수정된 제목");
+//		board.setContent("수정된 내용");
+//		board.setWriter("user00");
+//		
+//		int count = mapper.update(board);
+//		log.info("Update count : " + count);
+//	}
 }
