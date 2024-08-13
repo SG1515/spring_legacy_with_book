9db592d08f2a79dab649daba2098c1607a679aba
Seong_gu <sunggu507@naver.com>
2024-08-13 15:37:56 +0900

    feat :중복방지를 위한 uuid 적용

--- ex05.zip_expanded/ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05.zip_expanded/ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -3,6 +3,7 @@ package org.zerock.controller;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,8 +21,7 @@ public class UploadController {
 	public void uploadForm() {
 		log.info("upload form............");
 	}
-	
-	
+
 //	@PostMapping("/uploadFormAction")
 //	public void uploadFormPost(MultipartFile uploadFile, Model model) {
 //		log.info("file Name: " + uploadFile.getOriginalFilename());
@@ -35,13 +35,12 @@ public class UploadController {
 //			e.printStackTrace();
 //		}
 //	}
-	
 
 	@GetMapping("/uploadAjax")
 	public void uploadAjax() {
 		log.info("upload ajax");
 	}
-	
+
 	@PostMapping("/uploadAjaxAction")
 	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
 
@@ -53,16 +52,21 @@ public class UploadController {
 		if (uploadPath.exists() == false) {
 			uploadPath.mkdirs();
 		}
-		
-		
+
 		for (MultipartFile multipartFile : uploadFile) {
 
 			log.info("-------------------------------------");
 			log.info("Upload File Name: " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size: " + multipartFile.getSize());
 
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			// 중복 방지를 위한 UUID 적용
+			UUID uuid = UUID.randomUUID();
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+
 //			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
-			File saveFile = new File(uploadPath, multipartFile.getOriginalFilename());
+			File saveFile = new File(uploadPath, uploadFileName);
 
 			try {
 				multipartFile.transferTo(saveFile);
@@ -71,7 +75,7 @@ public class UploadController {
 			}
 		}
 	}
-	
+
 	/**
 	 * 년/월/일 폴더 생성
 	 * 
@@ -80,7 +84,7 @@ public class UploadController {
 	private String getFolder() {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		Date date = new Date(); 
+		Date date = new Date();
 		String str = sdf.format(date); // 형태에 맞는 날짜로 만들어진다.
 		return str.replace("-", File.separator);
 	}
